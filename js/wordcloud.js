002b305b4f770803f2c19727837f82367c148b3e
aarriiel <aarriiel@gmail.com>
2019-12-07 17:43:37 +0800

    add wordcloud/delete some useless

--- js/wordcloud.js
+++ js/wordcloud.js
@@ -1,31 +1,41 @@
-var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
-var lines = text.split(/[,\. ]+/g),
-    data = Highcharts.reduce(lines, function (arr, word) {
-        var obj = Highcharts.find(arr, function (obj) {
-            return obj.name === word;
-        });
-        if (obj) {
-            obj.weight += 1;
-        } else {
-            obj = {
-                name: word,
-                weight: 1
-            };
-            arr.push(obj);
-        }
-        return arr;
-    }, []);
+var facebook_min = "../data/png/facebook_min.png";
+var facebook_pre = "../data/png/facebook_pre.png";
+var facebook_res = "../data/png/facebook_res.png";
+var google_min = "../data/png/google_min.png";
+var google_pre = "../data/png/google_pre.png";
+var google_res = "../data/png/google_res.png";
+var apple_min = "../data/png/apple_min.png";
+var apple_pre = "../data/png/apple_pre.png";
+var apple_res = "../data/png/apple_res.png";
 
+var facebookObj = document.getElementById("facebook_image");
+var googleObj = document.getElementById("google_image");
+var appleObj = document.getElementById("apple_image");
+var facebookTitle = document.getElementById("facebook_title");
+var googleTitle = document.getElementById("google_title");
+var appleTitle = document.getElementById("apple_title");
 
-Highcharts.chart('google_min_key', {
-    series: [{
-        type: 'wordcloud',
-        plotBackgroundColor:'null',
-        plotBackgroundImage:'../data/facebook_icon.png',
-        data: data,
-        name: 'Occurrences'
-    }],
-    title: {
-        text: 'Wordcloud of Minimum Keyword'
-    }
-});
+$("#option-three").click(function(){
+    facebookObj.src = facebook_res;
+    googleObj.src = google_res;
+    appleObj.src = apple_res;
+    facebookTitle.innerHTML = "Responsibilities";
+    googleTitle.innerHTML = "Responsibilities";
+    appleTitle.innerHTML = "Responsibilities";
+})
+$("#option-two").click(function(){
+    facebookObj.src = facebook_pre;
+    googleObj.src = google_pre;
+    appleObj.src = apple_pre;
+    facebookTitle.innerHTML = "Preferred Quality";
+    googleTitle.innerHTML = "Preferred Quality";
+    appleTitle.innerHTML = "Preferred Quality";
+})
+$("#option-one").click(function(){
+    facebookObj.src = facebook_min;
+    googleObj.src = google_min;
+    appleObj.src = apple_min;
+    facebookTitle.innerHTML = "Minimum Quality";
+    googleTitle.innerHTML = "Minimum Quality";
+    appleTitle.innerHTML = "Minimum Quality";
+})
