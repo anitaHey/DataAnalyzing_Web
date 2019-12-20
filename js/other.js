bca5aafa95e5dabd528c6d4044f6be28c70155d7
anitaHey <jiaa.anita@gmail.com>
2019-12-21 05:01:34 +0800

    Change new UI.

--- js/other.js
+++ js/other.js
@@ -1,4 +1,62 @@
 $(document).ready(function() {
+    var svg_width = $('.head_nav_btn').width();
+
+    $('#video_svg').width(svg_width * 0.9);
+    $('#video_svg').height(svg_width);
+    $('#video_svg').css("margin-left", svg_width * 0.05);
+
+    var video_anima = bodymovin.loadAnimation({
+        container: document.getElementById('video_svg'),
+        path: 'https://assets9.lottiefiles.com/packages/lf20_YGNGxq.json',
+        animType: 'svg',
+        loop: true,
+        autoplay: false,
+    });
+    video_anima.setSpeed(0.6);
+
+    $('#chart_svg').width(svg_width);
+    $('#chart_svg').height(svg_width);
+    $('#chart_svg').css("margin-bottom", svg_width * 0.05);
+
+    var chart_anima = bodymovin.loadAnimation({
+        container: document.getElementById('chart_svg'),
+        path: 'https://assets9.lottiefiles.com/datafiles/2lgFyY8OHGMJPK0/data.json',
+        animType: 'svg',
+        loop: true,
+        autoplay: false,
+    });
+    chart_anima.goToAndStop(58, true);
+    chart_anima.setSpeed(0.8);
+
+    $('#about_svg').width(svg_width);
+    $('#about_svg').height(svg_width);
+
+    var about_anima = bodymovin.loadAnimation({
+        container: document.getElementById('about_svg'),
+        path: 'https://assets5.lottiefiles.com/packages/lf20_O711lP.json',
+        animType: 'svg',
+        loop: true,
+        autoplay: false,
+    });
+
+    $('#video_btn').hover(function() {
+        video_anima.play();
+    }, function() {
+        video_anima.goToAndStop(0, true);
+    });
+
+    $('#chart_btn').hover(function() {
+        chart_anima.play(0);
+    }, function() {
+        chart_anima.goToAndStop(58, true);
+    });
+
+    $('#about_btn').hover(function() {
+        about_anima.play();
+    }, function() {
+        about_anima.goToAndStop(0, true);
+    });
+
     $('input[name=question]').change(function() {
         var page = $(this).val();
 
@@ -75,5 +133,7 @@ $(document).ready(function() {
         $('.ans_div').each(function() {
             $(this).addClass('hide');
         });
+
+        $('input[name=question]').prop('checked',false);
     });
 });
